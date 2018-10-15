e1210692875d3aeea472d6cd49514a0bfdee4502
Alan Hong <xiupan@gmail.com>
2018-10-15 16:16:46 -0500

    Huzzah

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -4,13 +4,22 @@ class Genre < ActiveRecord::Base
 
   def song_count
     # return the number of songs in a genre
+    self.songs.count
   end
 
   def artist_count
     # return the number of artists associated with the genre
+    self.artists.count
   end
 
   def all_artist_names
     # return an array of strings containing every musician's name
+    new_array = []
+    object_array = self.artists
+    object_array.select do
+      |object|
+      new_array << object.name
+    end
+    new_array
   end
 end
